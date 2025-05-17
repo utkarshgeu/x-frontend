70a974c46d6ba2934f43d6ed71e44323ef81da77
Utkarsh Pandey <143593044+utkarshgeu@users.noreply.github.com>
2025-05-18 01:35:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "https://x-api.utkarshpandey.me",
         changeOrigin: true,
       }
     }
